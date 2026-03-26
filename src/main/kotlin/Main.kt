86c6aaf714457398592b97cd64fc26d6554fffac
nastosinka <osinkinanastasia@gmail.com>
2026-03-27 00:20:13 +0300

    feat: equation system and csv writer(?)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,35 @@
 package org.example
 
-fun main() {
-    println("Hello World!")
-    val calc = EquationSystem()
-    println("Sum: ${calc.sum(5, 5)}")
+import org.example.trigonometry.*
+import org.example.logarithm.*
+
+object Main {
+
+    private var outputDir: String =
+        System.getProperty("user.dir") + File.separator + "plots" + File.separator
+
+    private const val START = -10.0
+    private const val END = 10.0
+    private const val STEP = 0.01
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        generateFunctionData()
+    }
+
+    private fun generateFunctionData() {
+        CSVGraphWriter(Sine(), outputDir).write(START, END, STEP)
+        CSVGraphWriter(Cosine(), outputDir).write(START, END, STEP)
+        CSVGraphWriter(Secant(), outputDir).write(START, END, STEP)
+        CSVGraphWriter(Cosecant(), outputDir).write(START, END, STEP)
+        CSVGraphWriter(Tangent(), outputDir).write(START, END, STEP)
+        CSVGraphWriter(Cotangent(), outputDir).write(START, END, STEP)
+
+        CSVGraphWriter(NaturalLogarithm(), outputDir).write(START, END, STEP)
+        CSVGraphWriter(BaseNLogarithm(2.0), outputDir).write(START, END, STEP)
+        CSVGraphWriter(BaseNLogarithm(3.0), outputDir).write(START, END, STEP)
+        CSVGraphWriter(BaseNLogarithm(10.0), outputDir).write(START, END, STEP)
+
+        CSVGraphWriter(EquationSystem(), outputDir).write(START, END, STEP)
+    }
 }
